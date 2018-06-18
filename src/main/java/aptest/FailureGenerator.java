63a0163f4d694ce1c28e1bf5080accfe5390206a
Mateusz Zakarczemny <mateusz.zakarczemny@onzo.com>
2018-06-18 21:44:23 +0200

    fix attr name, print generated rand number

--- src/main/java/aptest/FailureGenerator.java
+++ src/main/java/aptest/FailureGenerator.java
@@ -6,13 +6,13 @@ import java.util.Random;
 public class FailureGenerator {
     static class ExpectedException extends RuntimeException {
     }
-
-    static Random rnd = new Random();
-
     static private Double threshold = 0.1d;
 
     static void failOrNot(String what) {
-        if (rnd.nextDouble() <= threshold) {
+        Random rnd = new Random();
+        double generated = rnd.nextDouble();
+        WriteToTmp.write(generated);
+        if (generated <= threshold) {
             WriteToTmp.write("Failing " + what + "! " + LocalDateTime.now());
             throw new ExpectedException();
         }
